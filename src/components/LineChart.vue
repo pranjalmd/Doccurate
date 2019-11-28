e1a6463c726067b36bca9a45412ed3e64b1aec91
Aniket Dhole <aniketrd@anikets-mbp.3500.dhcp.asu.edu>
2019-11-28 16:54:49 -0700

    Adding tooltip and modifying axis for volin plot

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -35,7 +35,7 @@ export default {
           // Build and Show the Y scale
           var y = d3.scaleLinear()
             .domain([ 3.5,8 ])          // Note that here the Y scale is set manually
-            .range([height, 0])
+            .range([height, 20])
           svg.append("g").call( d3.axisLeft(y) )
 
           // Build and Show the X scale. It is a band scale like for a boxplot: each group has an dedicated RANGE on the axis. This range has a length of x.bandwidth
@@ -43,10 +43,21 @@ export default {
             .range([ 0, width ])
             .domain(["setosa", "versicolor", "virginica"])
             .padding(0.05)     // This is important: it is the space between 2 groups. 0 means no padding. 1 is the maximum.
-          svg.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x))
 
+          var x_top = d3.scaleBand()
+            .range([ 0, width ])
+            .domain(["setosa", "versicolor", "virginica"])
+            .padding(0.05)     // This is important: it is the space between 2 groups. 0 means no padding. 1 is the maximum.
+
+
+          // svg.append("g")
+          //   .attr("transform", "translate(0," + height + ")")
+          //   .call(d3.axisBottom(x))
+
+          svg.append("g")
+            .attr("transform", "translate(0," + 10 + ")")
+            .attr("class", "axisRed")
+            .call(d3.axisTop(x_top))
           // Features of the histogram
           var histogram = d3.histogram()
                 .domain(y.domain())
@@ -79,6 +90,20 @@ export default {
             .range([0, x.bandwidth()])
             .domain([-maxNum,maxNum])
 
+          // This allows to find the closest X index of the mouse:
+          var bisect = d3.bisector(function(d) { return d.x; }).left;
+
+          // var tip = d3.tip()
+          //             .attr('class', 'd3-tip')
+          //             .offset([-10, 0])
+          //             .html(function(d) {
+          //               return "<strong>Frequency:</strong> <span style='color:red'>" + d.frequency + "</span>";
+          //             })
+          // svg.call(tip);
+
+          var div_selected = d3.select("#my_dataviz").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
           // Add the shape to this svg!
           svg
             .selectAll("myViolin")
@@ -91,82 +116,75 @@ export default {
                 .style("stroke", "none")
                 .style("fill","#69b3a2")
                 .attr("d", d3.area()
-                    .x0(function(d){ return(xNum(-d.length)) } )
+                    .x0(function(d){ 
+                      if (d.length == 0){
+                        return (xNum(0.1))
+                      }
+                      return(xNum(-d.length)) 
+                      } )
                     .x1(function(d){ return(xNum(d.length)) } )
                     .y(function(d){ return(y(d.x0)) } )
                     .curve(d3.curveCatmullRom)    // This makes the line smoother to give the violin appearance. Try d3.curveStep to see the difference
                 )
       
-                .on('mousemove', function (d, i) {
-                  console.log("Data", d3.event.pageY);
-                  console.log( d3.event.pageX, d3.event.pageY )
+                .on('mousemove', function (data,i) {
+                  console.log("Data", data);
+                  var mappedVales = data.map(x => x.x0);
+                  
+                  var y_scaled = y.invert(d3.mouse(this)[1]);
+                  console.log("Y" ,y_scaled, mappedVales);
+                  var index = d3.bisectLeft(mappedVales, y_scaled);
+                  var selectedData = mappedVales[index]
+                  console.log("Selected Index : ", index, selectedData);
+  
+                  div_selected.transition()
+                              .duration(200)
+                              .style("opacity", .9);
+                    div_selected.html("Hello" + "<br/>" + data[index].length)
+                      .style("left", (d3.event.pageX) + "px")
+                      .style("top", (d3.event.pageY) + "px");
+                })
+                .on("mouseout", function(n) { div_selected.transition()
+                                                .duration(500)
+                                                .style("opacity", 0);
                 });
-                
+
         })
 
     }
 };
 </script>
 
-<style scoped>
+<style>
 body {
   font: 10px sans-serif;
 }
 
-.axis path,
-.axis line,
-.axis1 path,
-.axis1 line {
+.axisRed .domain {
   fill: none;
-  stroke: #e6e7e8;
+  stroke: #3f3;
+  opacity: 0;
   shape-rendering: crispEdges;
 }
 
-.x.axis path,
-.x.axis1 path {
-  display: none;
-}
-
-.line {
+.axisRed .tick line {
   fill: none;
-  stroke-width: 1.5px;
-}
-
-.legend-box {
-  cursor: pointer;
-}
-
-#mouse-tracker {
-  stroke: #e6e7e8;
-  stroke-width: 1px;
-}
-
-.hover-line {
-  stroke: #e6e7e8;
-  fill: none;
-  stroke-width: 1px;
-  left: 10px;
-  shape-rendering: crispEdges;
-  opacity: 1e-6;
-}
-
-.hover-text {
-  stroke: none;
-  font-size: 30px;
-  font-weight: bold;
-  fill: #000000;
-}
-
-.tooltip {
-  font-weight: normal;
-}
-
-.brush .extent {
-  stroke: #fff;
+  stroke: #3f3;
+  opacity: 0;
   shape-rendering: crispEdges;
 }
 
-.area {
-  fill: lightsteelblue;
+div.tooltip {
+  position: absolute;
+  text-align: center;
+  width: 60px;
+  height: 28px;
+  padding: 2px;
+  font: 12px sans-serif;
+  background: lightsteelblue;
+  border: 0px;
+  border-radius: 8px;
+  pointer-events: none;
 }
+
 </style>
