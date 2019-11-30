c2bcac62900e778ca08b935dc97f2c8d588a6582
piyushdoke24@gmail.com <piyushdoke24@gmail.com>
2019-11-30 16:09:19 -0700

    tree

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -273,7 +273,7 @@ export default {
         })
         .style("fill", "green");
       //horizontal bar chart end
-    });//horizontal bar chart csv closed
+    }); //horizontal bar chart csv closed
 
     //patient info tab dynamic property
     d3.select("#patientinfo")
@@ -307,33 +307,250 @@ export default {
           return d.val;
         })
         .style("font-weight", "normal")
-        .style("text-align", "left")
-        
+        .style("text-align", "left");
+
       //patient info tab dynamic property end
-    });//patient_info csv closed
+    }); //patient_info csv closed
 
-    d3.csv("Patient1_text.csv", function(error, data) {
+    // //patient text details
+    // d3.csv("Patient1_text.csv", function(error, data) {
+    //   if (error) throw error;
+
+    //   d3.select("#textdetails")
+    //     .selectAll("h6")
+    //     .data(data)
+    //     .enter()
+    //     .append("h6")
+    //     .text(function(d) {
+    //       return d.key + ": ";
+    //     })
+    //     .style("text-align", "left")
+    //     .style("margin-top", "3%")
+    //     .style("font-weight", "bold")
+    //     .append()
+    //     .text(function(d) {
+    //       return d.val;
+    //     })
+    //     .style("font-weight", "normal");
+    // }); //patient text data csv closed
+
+    //tree taxonomy
+
+    d3.select("#textdetails")
+      .selectAll("*")
+      .remove();
+
+    d3.json("temp.json", function(error, flare) {
       if (error) throw error;
+      root = d3.hierarchy(flare);
+      root.x0 = 0;
+      root.y0 = 0;
+      update(root);
+    });
+    //============================================================
+    // Set the dimensions and margins of the diagram
+    var margintree = { top: 20, right: 90, bottom: 30, left: 90 },
+      widthtree = 550 - margintree.left - margintree.right,
+      heighttree = 500 - margintree.top - margintree.bottom;
 
-      d3.select("#textdetails")
-        .selectAll("h6")
-        .data(data)
+    // append the svg object to the body of the page
+    // appends a 'group' element to 'svg'
+    // moves the 'group' element to the top left margintree
+    var svgtree = d3
+      .select("#textdetails")
+      .append("svg")
+      .attr("width", widthtree + margintree.right + margintree.left)
+      .attr("height", heighttree + margintree.top + margintree.bottom)
+      .attr("transform", "rotate(90)")
+      .append("g")
+      .attr("transform", "translate(" + margintree.left + "," + margintree.top + ")");
+      
+
+    var i = 0,
+      duration = 750,
+      root;
+
+    // declares a tree layout and assigns the size
+    var treemap = d3.tree().size([heighttree, widthtree]);
+
+    // Assigns parent, children, height, depth
+    root = d3.hierarchy(treeData, function(d) {
+      return d.children;
+    });
+    root.x0 = heighttree / 2;
+    root.y0 = 0;
+
+    // Collapse after the second level
+    root.children.forEach(collapse);
+
+    update(root);
+
+    // Collapse the node and all it's children
+    function collapse(d) {
+      if (d.children) {
+        d._children = d.children;
+        d._children.forEach(collapse);
+        d.children = null;
+      }
+    }
+
+    function update(source) {
+      // Assigns the x and y position for the nodes
+      var treeData = treemap(root);
+
+      // Compute the new tree layout.
+      var nodes = treeData.descendants(),
+        links = treeData.descendants().slice(1);
+
+      // Normalize for fixed-depth.
+      nodes.forEach(function(d) {
+        d.y = d.depth * 110;
+      });
+
+      // ****************** Nodes section ***************************
+
+      // Update the nodes...
+      var node = svgtree.selectAll("g.node").data(nodes, function(d) {
+        return d.id || (d.id = ++i);
+      });
+
+      // Enter any new modes at the parent's previous position.
+      var nodeEnter = node
         .enter()
-        .append("h6")
+        .append("g")
+        .attr("class", "node")
+        .attr("transform", function(d) {
+          return "translate(" + source.y0 + "," + source.x0 + ")";
+        })
+        .on("click", click);
+
+      // Add Circle for the nodes
+      nodeEnter
+        .append("circle")
+        .attr("class", "node")
+        .attr("r", 1e-6)
+        .style("fill", function(d) {
+          return d._children ? "lightsteelblue" : "#fff";
+        });
+
+      // Add labels for the nodes
+      nodeEnter
+        .append("text")
+        .attr("dy", ".35em")
+        .attr("x", function(d) {
+          return d.children || d._children ? -13 : 13;
+        })
+        .attr("text-anchor", function(d) {
+          return d.children || d._children ? "end" : "start";
+        })
         .text(function(d) {
-          return d.key + ": ";
+          return d.data.name;
+        });
+
+      // UPDATE
+      var nodeUpdate = nodeEnter.merge(node);
+
+      // Transition to the proper position for the node
+      nodeUpdate
+        .transition()
+        .duration(duration)
+        .attr("transform", function(d) {
+          return "translate(" + d.y + "," + d.x + ")";
+        });
+
+      // Update the node attributes and style
+      nodeUpdate
+        .select("circle.node")
+        .attr("r", 10)
+        .style("fill", function(d) {
+          return d._children ? "lightsteelblue" : "#fff";
         })
-        .style("text-align", "left")
-        .style("margin-top", "3%")
-        .style("font-weight", "bold")
-        .append()
-        .text(function(d){
-          return d.val;
+        .attr("cursor", "pointer");
+
+      // Remove any exiting nodes
+      var nodeExit = node
+        .exit()
+        .transition()
+        .duration(duration)
+        .attr("transform", function(d) {
+          return "translate(" + source.y + "," + source.x + ")";
         })
-        .style("font-weight", "normal");
-        
-    });//patient text data csv closed
-    
+        .remove();
+
+      // On exit reduce the node circles size to 0
+      nodeExit.select("circle").attr("r", 1e-6);
+
+      // On exit reduce the opacity of text labels
+      nodeExit.select("text").style("fill-opacity", 1e-6);
+
+      // ****************** links section ***************************
+
+      // Update the links...
+      var link = svgtree.selectAll("path.link").data(links, function(d) {
+        return d.id;
+      });
+
+      // Enter any new links at the parent's previous position.
+      var linkEnter = link
+        .enter()
+        .insert("path", "g")
+        .attr("class", "link")
+        .attr("d", function(d) {
+          var o = { x: source.x0, y: source.y0 };
+          return diagonal(o, o);
+        });
+
+      // UPDATE
+      var linkUpdate = linkEnter.merge(link);
+
+      // Transition back to the parent element position
+      linkUpdate
+        .transition()
+        .duration(duration)
+        .attr("d", function(d) {
+          return diagonal(d, d.parent);
+        });
+
+      // Remove any exiting links
+      var linkExit = link
+        .exit()
+        .transition()
+        .duration(duration)
+        .attr("d", function(d) {
+          var o = { x: source.x, y: source.y };
+          return diagonal(o, o);
+        })
+        .remove();
+
+      // Store the old positions for transition.
+      nodes.forEach(function(d) {
+        d.x0 = d.x;
+        d.y0 = d.y;
+      });
+
+      // Creates a curved (diagonal) path from parent to the child nodes
+      function diagonal(s, d) {
+        path = `M ${s.y} ${s.x}
+            C ${(s.y + d.y) / 2} ${s.x},
+              ${(s.y + d.y) / 2} ${d.x},
+              ${d.y} ${d.x}`;
+
+        return path;
+      }
+
+      // Toggle children on click.
+      function click(d) {
+        if (d.children) {
+          d._children = d.children;
+          d.children = null;
+        } else {
+          d.children = d._children;
+          d._children = null;
+        }
+        update(d);
+      }
+    }
+    //================================
   } //end of the mounted function
 };
 </script>
